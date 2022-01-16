798a4a85bc7ea8963525500f24cc65997b8bf3d8
stardust <lql9705@gmail.com>
2022-01-16 17:56:41 +0800

    qexpr

--- src/parsing.c
+++ src/parsing.c
@@ -5,12 +5,16 @@
 #include <errno.h>
 #include "mpc.h"
 
-enum { LVAL_NUM, LVAL_ERR, LVAL_FNUM, LVAL_SYM, LVAL_SEXPR};
+enum { LVAL_NUM, LVAL_ERR, LVAL_FNUM, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR};
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM, LERR_INVALID_OP_FMOD };
 
 #define EXTRACT_NUM(x) (long)((x)->type == LVAL_NUM ? (x)->num : (x)->fnum)
 #define EXTRACT_FNUM(x) (double)((x)->type == LVAL_NUM ? (x)->num : (x)->fnum)
 #define EXTRACT_VALUE(x) ((x)->type == LVAL_NUM ? (x)->num : (x)->fnum)
+
+#define LASSERT(args, cond, err)                                               \
+  if (!(cond)) { lval_del(args); return lval_err((err)); }
+
 
 struct lval_s {
   int type;
@@ -26,6 +30,17 @@ struct lval_s {
 
 typedef struct lval_s lval;
 
+lval* lval_eval(lval *v);
+  
+lval*
+lval_qexpr(void)
+{
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_QEXPR;
+  v->count = 0;
+  v->cell = NULL;
+  return v;
+}
 
 lval*
 lval_num(long x)
@@ -89,6 +104,7 @@ lval_del(lval *v)
     free(v->err);
     break;
   case LVAL_SEXPR:
+  case LVAL_QEXPR:
     for (int i = 0; i < v->count; i++) {
       lval_del(v->cell[i]);
     }
@@ -142,11 +158,15 @@ lval_read(mpc_ast_t *t) {
   // root
   if (strcmp(t->tag, ">") == 0 || strstr(t->tag, "sexpr")) {
     x = lval_sexpr();
+  } else if (strstr(t->tag, "qexpr")) {
+    x = lval_qexpr();
   }
-  
+
   for (int i = 0; i < t->children_num; i++) {
     if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
     if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, "}") == 0) { continue; }
     if (strcmp(t->children[i]->tag, "regex") == 0) { continue; }
     x = lval_add(x, lval_read(t->children[i]));
   }
@@ -175,6 +195,9 @@ lval_print(lval *v)
   case LVAL_SEXPR:
     lval_expr_print(v, '(', ')');
     break;
+  case LVAL_QEXPR:
+    lval_expr_print(v, '{', '}');
+    break;    
   default:
      break;    
   }
@@ -343,6 +366,74 @@ eval_unary(const char *op, lval *a)
 }
 
 lval*
+builtin_head(lval *a)
+{
+  LASSERT(a, a->count == 1, "Function 'head' accepts 1 argument.");
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'head' expects QEXPR argument.");
+  LASSERT(a, a->cell[0]->count != 0, "Function 'head' passed {}!");
+
+  lval* v = lval_take(a, 0);
+  while (v->count > 1) {
+    lval_del(lval_pop(v, 1));
+  }
+  return v;
+}
+
+lval*
+builtin_tail(lval *a)
+{
+  LASSERT(a, a->count == 1, "Function 'tail' accepts 1 argument.");
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'tail' expects QEXPR.");
+  LASSERT(a, a->cell[0]->count != 0, "Function 'tail' passed {}!");
+  
+  lval* v = lval_take(a, 0);
+  lval_del(lval_pop(v, 0));
+  return v;
+}
+
+lval*
+builtin_list(lval *a)
+{
+  a->type = LVAL_QEXPR;
+  return a;
+}
+
+lval*
+builtin_eval(lval *a)
+{
+  LASSERT(a, a->count == 1, "Function 'eval' accepts 1 argument.");
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'eval' expects QEXPR.");
+
+  lval *x = lval_take(a, 0);
+  x->type = LVAL_SEXPR;
+  return lval_eval(x);
+}
+
+lval*
+lval_join(lval *a, lval *b)
+{
+  while (b->count) {
+    a = lval_add(a, lval_pop(b, 0));
+  }
+  lval_del(b);
+  return a;
+}
+
+lval*
+builtin_join(lval *a)
+{
+  for (int i = 0; i < a->count; i++) {
+    LASSERT(a, a->cell[i]->type == LVAL_QEXPR, "Function join expects QEXPR.");
+  }
+  lval *x = lval_pop(a, 0);
+  while (a->count) {
+    x = lval_join(x, lval_pop(a, 0));
+  }
+  lval_del(a);
+  return x;
+}
+
+lval*
 builtin_op(lval *v, const char *op)
 {
   for (int i = 0; i < v->count; i++) {
@@ -377,7 +468,23 @@ builtin_op(lval *v, const char *op)
   return x;
 }
 
-lval* lval_eval(lval *v);
+lval*
+builtin(lval *a, const char *func)
+{
+  if (strcmp(func, "head") == 0) {
+    return builtin_head(a);
+  } else if (strcmp(func, "tail") == 0) {
+    return builtin_tail(a);
+  } else if (strcmp(func, "list") == 0) {
+    return builtin_list(a);
+  } else if (strcmp(func, "join") == 0) {
+    return builtin_join(a);
+  } else if (strcmp(func, "eval") == 0) {
+    return builtin_eval(a);
+  } else {
+    return builtin_op(a, func);
+  }
+}
 
 lval*
 lval_eval_sexpr(lval *v)
@@ -407,8 +514,8 @@ lval_eval_sexpr(lval *v)
     return lval_err("s-expression must start with symbol");
   }
 
-  lval* result = builtin_op(v, f->sym);
-  lval_del(f);
+  lval* result = builtin(v, f->sym);
+  lval_del(f); 
   return result;
 }
 
@@ -428,25 +535,33 @@ main(int argc, char **argv)
   puts("Press Ctrl+c to exit\n");
   
   mpc_parser_t* number   = mpc_new("number");
+
   mpc_parser_t* fnumber   = mpc_new("fnumber");  
   mpc_parser_t* symbol = mpc_new("symbol");
   mpc_parser_t* sexpr     = mpc_new("sexpr");  
   mpc_parser_t* expr     = mpc_new("expr");
   mpc_parser_t* lispy    = mpc_new("lispy");
-  
-  mpca_lang(MPCA_LANG_DEFAULT,
-  "                                                                      \
-  number   : /-?[0-9]+/ ;                                                \
-  fnumber   : /-?[0-9]+\\.[0-9]+/ ;                                         \
-  symbol : '%' | '+' | '-' | '*' | '/' | '^' | \"min\" | \"max\";      \
-  sexpr    : '(' <expr>* ')' ; \
-  expr     : <fnumber> | <number> | <symbol> | <sexpr> ; \
-  lispy    : /^/ <expr>+ /$/ ;            \
-  ",
-  number, fnumber, symbol, sexpr, expr, lispy);
+  mpc_parser_t* qexpr    = mpc_new("qexpr");
+
+  mpca_lang(
+      MPCA_LANG_DEFAULT,
+      "                                                                     \
+      number   : /-?[0-9]+/ ;                                               \
+      fnumber  : /-?[0-9]+\\.[0-9]+/ ;                                      \
+      symbol   : '%' | '+' | '-' | '*' | '/' | '^' | \"min\" | \"max\"      \
+               | \"tail\" | \"list\" | \"head\" | \"eval\" | \"join\";      \
+      sexpr    : '(' <expr>* ')' ;                                          \
+      qexpr    : '{' <expr>* '}' ;                                          \
+      expr     : <fnumber> | <number> | <symbol> | <sexpr> | <qexpr>;       \
+      lispy    : /^/ <expr> + /$/ ; ",
+      number, fnumber, symbol, sexpr, qexpr, expr, lispy);
 
   while (1) {
     char *input = readline("lispy> ");
+    if (!input) {
+      printf("\nexit\n");
+      break;
+    }
     add_history(input);
 
     mpc_result_t r;
@@ -465,6 +580,6 @@ main(int argc, char **argv)
 
     free(input);
   }
-  mpc_cleanup(6, number, fnumber, symbol, expr, sexpr, lispy);
+  mpc_cleanup(7, number, fnumber, symbol, expr, sexpr, lispy, qexpr);
   return 0;
 }
